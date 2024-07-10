55282b408ac8f8b9356fb88c0cbb7e630a0da681
Denise Pingiori <pingiorid@gmail.com>
2024-07-10 17:01:28 +0200

    [upd] upd width and height at square

--- js/script.js
+++ js/script.js
@@ -3,6 +3,10 @@ function generateRandomNumbers(max){
     return Math.floor(Math.random() * max + 1);
 }
 
+//funzione che mi genera array bombe, creiamo e restituiamo un array
+function generateBombsList(nums, NUM_OF_BOMBS) {
+
+}
 
 //funzione crea casella della griglia
 function createSingleSquare(num){
@@ -11,6 +15,11 @@ function createSingleSquare(num){
 
     //aggiungo classe square al div
     square.classList.add('square');
+
+    //determino la width e la height del quadrato 
+    let sideLenght = `calc(100% ${sideNumber})`;
+    square.style.width = sideLenght;
+    square.style.height = sideLenght;
 
     //aggiungo numero all'interno del div
     square.innerText = num + 1;
@@ -38,11 +47,6 @@ button.addEventListener('click', function(){
     //recupero elem che dovrà contenere la griglia dal dom
     const grid = document.getElementById('grid');
 
-  
-
-    //FUNZIONE CHE GENERA CASELLE
-    generateGrid(100,10)
-
     //ciclo x creare le 100 caselle
     for(let i = 0; i<100; i++ ) {
         //chiamo la funzione x creare le caselle passandole come indice attuale x scriverci dentro
@@ -56,13 +60,21 @@ button.addEventListener('click', function(){
 //funzione che genera nuova partita 
 function createNewGame(){
 
-    //array che contiene le bombe
-    let bombs = [];
+    
     //variabile punteggio partita
     let points = 0;
     //variabile che stabilisce che la partita è finita
     let gameOver = false;
-    
+    //definizione cost che mi indica il num di bombe dell'array
+    const NUM_OF_BOMBS = 16; 
+
      //svuoto la griglia da tutti i suoi elementi interni
       grid.innerHTML = ' ';
+
+    //genero le bombe e le inserisco nell'array preposto al contenimento delle bombe
+    const bombs = generateBombsList(100, NUM_OF_BOMBS);
+
+    //FUNZIONE CHE GENERA CASELLE
+    generateGrid(100,10);
+
 }
